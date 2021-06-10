0877a285e6d03430f535a57d850fa28e0c82aff2
Svetlana Polozova <svetabjs@gmail.com>
2021-06-10 15:16:15 +0500

    add message status

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,5 +1,5 @@
 // @ts-check
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useFormik } from 'formik';
 import {
   Form,
@@ -36,7 +36,7 @@ const Messages = () => {
   const messagesListRef = useRef(null);
   const dispatch = useDispatch();
   const socket = useSocket();
-  // const [state, setState] = useState(null);
+  const [isSending, setIsSending] = useState(false);
 
   useEffect(() => {
     messageInputRef.current.focus();
@@ -74,6 +74,7 @@ const Messages = () => {
       body: '',
     },
     onSubmit: ({ body }) => {
+      setIsSending(true);
       const { username } = JSON.parse(localStorage.getItem('userId'));
       const outgoingMessage = {
         body,
@@ -83,12 +84,13 @@ const Messages = () => {
       socket.volatile.emit('newMessage', outgoingMessage, (response) => {
         console.log(response);
         if (response.status === 'ok') {
+          setIsSending(false);
           formik.resetForm();
           messageInputRef.current.focus();
           return;
         }
-        socket.off('newMassage');
         messageInputRef.current.focus();
+        setIsSending(false);
       });
     },
   });
@@ -121,9 +123,9 @@ const Messages = () => {
                 value={formik.values.body}
                 onChange={formik.handleChange}
                 onBlur={formik.handleBlur}
-                disabled={formik.isSubmitting}
+                disabled={isSending}
               />
-              <button className="btn btn-group-vertical" type="submit" disabled={formik.isSubmitting}>
+              <button className="btn btn-group-vertical" type="submit" disabled={isSending}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" className="bi bi-arrow-right-square" viewBox="0 0 16 16">
                   <path d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm4.5 5.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z" />
                 </svg>
