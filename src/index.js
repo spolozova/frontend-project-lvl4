a20a52db4f846b1c8c7647a4a984017e68612bea
Svetlana Polozova <svetabjs@gmail.com>
2021-06-22 10:55:28 +0500

    fix entry point

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // @ts-check
 import ReactDOM from 'react-dom';
-import Rollbar from 'rollbar';
+// import Rollbar from 'rollbar';
 import init from './init.jsx';
 import '../assets/application.scss';
 
@@ -9,12 +9,12 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 // не используется проверка на NODE_ENV т.к. Rollbar access token существует только в production
-const rollbar = new Rollbar();
-rollbar.configure({
-  accessToken: '3304fc7414d24faabb91eb38a1e718da',
-  captureUncaught: true,
-  captureUnhandledRejections: true,
-});
+// const rollbar = new Rollbar();
+// rollbar.configure({
+//   accessToken: '3304fc7414d24faabb91eb38a1e718da',
+//   captureUncaught: true,
+//   captureUnhandledRejections: true,
+// });
 
 const render = async () => {
   const dom = await init();
