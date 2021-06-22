74f0c3677219d6098306819d3ac5c243aa6bdb24
Svetlana Polozova <svetabjs@gmail.com>
2021-06-22 12:06:18 +0500

    update index.js

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -11,6 +11,7 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx'],
   },
+  entry: './src/index.js',
   output: {
     path: path.join(__dirname, 'dist', 'public'),
     publicPath: '/assets/',
