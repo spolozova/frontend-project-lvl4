e840c748e47e156cffa212a5c5028e5d06a6fee0
Svetlana Polozova <svetabjs@gmail.com>
2021-06-26 12:57:37 +0500

    fix channels

--- src/components/Channels.jsx
+++ src/components/Channels.jsx
@@ -103,13 +103,13 @@ const Channels = () => {
     <Col className="col-4 col-md-2 border-end pt-5 px-0 bg-light">
       <div className="d-flex justify-content-between mb-2 ps-4 px-4">
         <span>{t('chatPage.channels')}</span>
-        <button type="button" name="+" className="p-0 text-primary btn btn-group-vertical" onClick={() => dispatch(openModal({ type: 'addChannel' }))}>
+        <button type="button" className="p-0 text-primary btn btn-group-vertical" onClick={() => dispatch(openModal({ type: 'addChannel' }))}>
           <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" className="bi bi-plus-square" viewBox="0 0 16 16">
             <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
             <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
           </svg>
+          <span className="visually-hidden">+</span>
         </button>
-        <span className="visually-hidden">+</span>
       </div>
       <Nav className="flex-column nav-pills nav-fill" as="ul">
         {channels.map(renderChannelsList)}
